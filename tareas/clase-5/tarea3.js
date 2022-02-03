b782c5ca26cc5252ec8b1cb4356ba706d942102c
Roman Descotte <55287661+sk8mumy@users.noreply.github.com>
2022-02-03 20:05:34 -0300

    Agrega tercer tarea de clase 5

---
+++ tareas/clase-5/tarea3.js
@@ -0,0 +1,52 @@
+//TAREA: En otro archivo distinto,
+// Por cada clase de r/argentina programa existente, vamos a pedir:
+// horas, minutos y segundos de cada video. Ej. Si un video dura
+// 2 horas, 38 minutos y 20 segundos, vamos a rellenar 3 campos de texto con
+// cada dato.
+// al apretar el botón "Calcular tiempo total", debe mostrar en un
+// <strong> pre-creado el tiempo total de los videos.
+
+const $horasClases = document.querySelectorAll(".horas");
+const calcularTotalHoras = function () {
+    let resultado = 0; 
+    for(let i = 0; i < $horasClases.length; i++) {
+        resultado = resultado + Number($horasClases[i].value);
+    }
+    console.log("El total de horas es: " + resultado)
+    return resultado;
+}
+
+const $minutosClases = document.querySelectorAll(".minutos");
+const calcularTotalMinutos = function() {
+    let resultado = 0;
+    for(let i = 0; i < $minutosClases.length; i++) {
+        resultado = resultado + Number($minutosClases[i].value);
+    }
+    console.log("El total de minutos es: " + resultado)
+    return resultado;
+}
+
+const $segundosClases = document.querySelectorAll(".segundos");
+const calcularTotalSegundos = function() {
+    let resultado = 0;
+    for(let i = 0; i < $segundosClases.length; i++) {
+        resultado = resultado + Number($segundosClases[i].value);
+    }
+    console.log("El total de segundos es:" + resultado);
+    return resultado;
+}
+
+function mostrarTotalTiempo() {
+    const $tiempoTotal = document.querySelector("#tiempo-total");
+    $tiempoTotal.textContent = `El tiempo total de las clases es de: ${calcularTotalHoras()} horas, ${calcularTotalMinutos()} minutos y ${calcularTotalSegundos()} segundos`;
+}
+
+const $btnCalcularTiempo = document.querySelector("#btn-calcular-tiempo");
+$btnCalcularTiempo.onclick = function() {
+    mostrarTotalTiempo();
+    return false;
+}
+
+
+
+
